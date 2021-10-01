1f843113c42d4768aee6facc86af1da27b9d4b7f
Igor Gaponenko <gapon@slac.stanford.edu>
2021-10-01 18:59:37 -0500

    Database selectors on the ingest pages "Status" and "Transactions"

--- www/qserv/js/IngestTransactions.js
+++ www/qserv/js/IngestTransactions.js
@@ -13,10 +13,6 @@ function(CSSLoader,
 
     class IngestTransactions extends FwkApplication {
 
-
-        /// @returns the default update interval for the page
-        static update_ival_sec() { return 10; }
-
         constructor(name) {
             super(name);
         }
@@ -35,13 +31,13 @@ function(CSSLoader,
         /// @see FwkApplication.fwk_app_on_update
         fwk_app_on_update() {
             if (this.fwk_app_visible) {
+                this._init();
                 if (this._prev_update_sec === undefined) {
                     this._prev_update_sec = 0;
                 }
                 let now_sec = Fwk.now().sec;
-                if (now_sec - this._prev_update_sec > IngestTransactions.update_ival_sec()) {
+                if (now_sec - this._prev_update_sec > this._update_interval_sec()) {
                     this._prev_update_sec = now_sec;
-                    this._init();
                     this._load();
                 }
             }
@@ -59,12 +55,29 @@ function(CSSLoader,
             /* <span style="color:maroon">&sum;</span>&nbsp; */
 
             let html = `
+<div class="form-row">
+  <div class="form-group col-md-3">
+    <label for="trans-database">Database:</label>
+    <select id="trans-database" class="form-control form-control-view">
+    </select>
+  </div>
+  <div class="form-group col-md-1">
+    <label for="trans-update-interval">Interval <i class="bi bi-arrow-repeat"></i></label>
+    <select id="trans-update-interval" class="form-control form-control-view">
+    <option value="10">10 sec</option>
+    <option value="20">20 sec</option>
+    <option value="30" selected>30 sec</option>
+    <option value="60">1 min</option>
+    <option value="120">2 min</option>
+    <option value="300">5 min</option>
+    </select>
+  </div>
+</div>
 <div class="row">
   <div class="col">
     <table class="table table-sm table-hover table-bordered" id="fwk-ingest-transactions">
       <thead class="thead-light">
         <tr>
-          <th rowspan="2">database</th>
           <th rowspan="2" class="right-aligned">id</th>
           <th rowspan="2" class="center-aligned">state</th>
           <th rowspan="2">begin time</th>
@@ -86,101 +99,134 @@ function(CSSLoader,
     </table>
   </div>
 </div>`;
-            this.fwk_app_container.html(html);
+            let cont = this.fwk_app_container.html(html);
+            cont.find(".form-control-view").change(() => {
+                this._load();
+            });
+            this._disable_databases(true);
         }
-        
-        /// @returns JQuery table object displaying the transactions
+        _form_control(elem_type, id) {
+            if (this._form_control_obj === undefined) this._form_control_obj = {};
+            if (!_.has(this._form_control_obj, id)) {
+                this._form_control_obj[id] = this.fwk_app_container.find(elem_type + '#' + id);
+            }
+            return this._form_control_obj[id];
+        }
+        _get_database() { return this._form_control('select', 'trans-database').val(); }
+        _set_database(val) { this._form_control('select', 'trans-database').val(val); }
+        _set_databases(databases) {
+            // Keep the current selection after updating the selector
+            const current_database = this._get_database();
+            this._form_control('select', 'trans-database').html(
+                _.reduce(
+                    databases,
+                    (html, name) => {
+                        const selected = !html ? 'selected' : ''; 
+                        return html + `<option value="${name}" ${selected}>${name}</option>`;
+                    },
+                    ''
+                )
+            );
+            if (current_database) this._set_database(current_database);
+        }
+        _disable_databases(disable) {
+            this._form_control('select', 'trans-database').prop('disabled', disable);
+        }
+        _update_interval_sec() { return this._form_control('select', 'trans-update-interval').val(); }
         _table() {
             if (this._table_obj === undefined) {
                 this._table_obj = this.fwk_app_container.find('table#fwk-ingest-transactions');
             }
             return this._table_obj;
         }
+        _status() {
+            if (this._status_obj === undefined) {
+                this._status_obj = this._table().children('caption');
+            }
+            return this._status_obj;
+        }
 
-        /**
-         * Load data from a web servie then render it to the application's page.
-         */
+        /// Load data from a web service then render it to the application's page.
         _load() {
             if (this._loading === undefined) this._loading = false;
             if (this._loading) return;
             this._loading = true;
 
-            this._table().children('caption').addClass('updating');
-
+            this._status().addClass('updating');
+            this._disable_databases(true);
+            this._load_databases();
+        }
+        _load_databases() {
+            Fwk.web_service_GET(
+                "/replication/config",
+                {},
+                (data) => {
+                    if (!data.success) {
+                        this._on_failure(data.error);
+                        return;
+                    }
+                    this._set_databases(_.map(
+                        _.sortBy(data.config.databases, function (info) { return info.is_published; }),
+                        function (info) { return info.database; }
+                    ));
+                    this._load_transactions();
+                },
+                (msg) => { this._on_failure(msg); }
+            );
+        }
+        _load_transactions() {
+            const current_database = this._get_database();
+            if (!current_database) {
+                this._on_failure("No databases exist yet in this instance of Qserv");
+                return;
+            }
             Fwk.web_service_GET(
                 "/ingest/trans",
-                {family: '', all_databases: 0, is_published: 0, contrib: 1, contrib_long: 0},
+                {database: current_database, contrib: 1, contrib_long: 0},
                 (data) => {
-                    this._display(data.databases);
-                    Fwk.setLastUpdate(this._table().children('caption'));
-                    this._table().children('caption').removeClass('updating');
+                    if (!data.success) {
+                        this._on_failure(msg);
+                        return;
+                    }
+                    this._display(data.databases[current_database]);
+                    this._disable_databases(false);
+                    Fwk.setLastUpdate(this._status());
+                    this._status().removeClass('updating');
                     this._loading = false;
                 },
-                (msg) => {
-                    console.log('request failed', this.fwk_app_name, msg);
-                    this._table().children('caption').html('<span style="color:maroon">No Response</span>');
-                    this._table().children('caption').removeClass('updating');
-                    this._loading = false;
-                }
+                (msg) => { this._on_failure(msg); }
             );
+        }
+        _on_failure(msg) {
+            this._status().html(`<span style="color:maroon">${msg}</span>`);
+            this._table().html('');
+            this._status().removeClass('updating');
+            this._loading = false;
         }
 
         /**
          * Render the data received from a server
-         * @param {Object} databases  transactions and other relevant info for unpublished databases
+         * @param {Object} databases  transactions and other relevant info for the select database
          */
-        _display(databases) {
-
-            /* Translate a dictionary of database descriptors into a reverse sorted
-             * array of the descriptors. The elements of the resulting array will be stored
-             * by the maximum value of the transaction identifiers in the descending order.
-             * The idea here is to show newer databases on the top of the table.
-             */
-            let listOfDatabases = _.sortBy(
-                /* The mapping function will return an array of descriptors */
-                _.map(
-                    databases,
-                    function(databaseInfo, database) {
-                        // Inject database name into the object since it will be lost
-                        // after translating the dictonary into a list. Note that database
-                        // names serve as keys in the dictionary.
-                        databaseInfo.name = database;
-                        return databaseInfo;
-                    }
-                ),
-                function(databaseInfo) {
-                    /* The reduction function will return the maximum transaction identifier,
-                     * or 0 of no transactions were recorded. */
-                    return _.reduce(
-                        databaseInfo.transactions,
-                        function(maxTransactionId, transaction) { return Math.max(maxTransactionId, transaction.id); },
-                        0
-                    );
-                }
-            ).reverse();
-            console.log(listOfDatabases);
+        _display(databaseInfo) {
             let html = '';
-            for (let databaseIdx in listOfDatabases) {
-                let databaseInfo = listOfDatabases[databaseIdx];
-                let database = databaseInfo.name;
-                if (databaseInfo.transactions.length === 0) {
-                    html += `
-<tr>
-  <th rowspan="2"><pre>${database}</pre></th>
-</tr>
+            if (databaseInfo.transactions.length === 0) {
+                html += `
 <tr>
   <th>&nbsp;</th>
   <td>&nbsp;</th>
   <td>&nbsp;</th>
   <td>&nbsp;</th>
 </tr>`;
-                } else {
-                    html += `
-<tr>
-  <th rowspan="${databaseInfo.transactions.length+1}"><pre>${database}</pre></th>
-</tr>`;
-                    for (let transactionIdx in databaseInfo.transactions) {
-                        let transactionInfo = databaseInfo.transactions[transactionIdx];
+            } else {
+                // The collection of all transaction identifiers in a scope of the current database
+                // will be passed to the conbtributions page when a click on a transaction is made.
+                this._transactions = [];
+                html += _.reduce(
+                    // Transactions are shown sorted in the ASC order
+                    _.sortBy(databaseInfo.transactions, function (info) { return info.id; }),
+                    (html, transactionInfo) => {
+                        this._transactions.push(transactionInfo.id);
                         let transactionCssClass = 'bg-white';
                         switch (transactionInfo.state) {
                             case 'STARTED':  transactionCssClass = 'bg-transparent'; break;
@@ -196,7 +242,7 @@ function(CSSLoader,
                         let numFiles = numRegular + numChunks + numChunkOverlaps;
                         let numRows = transactionInfo.contrib.summary.num_rows;
                         let dataSize = transactionInfo.contrib.summary.data_size_gb.toFixed(2);
-                        html += `
+                        return html + `
 <tr class="transaction" id="${transactionInfo.id}">
   <th class="right-aligned"><pre>${transactionInfo.id}</pre></th>
   <td class="center-aligned ${transactionCssClass}"><pre>${transactionInfo.state}</pre></th>
@@ -210,16 +256,18 @@ function(CSSLoader,
   <td class="right-aligned"><pre>${numRows}</pre></th>
   <td class="right-aligned"><pre>${dataSize}</pre></th>
 </tr>`;
-                    }
-                }
+                    },
+                    ''
+                );
             }
-            let displayContributions = function(e) {
-                let tr = $(e.currentTarget);
-                let transactionId = tr.attr("id");
-                Fwk.show("Ingest", "Contributions");
-                Fwk.current().loadTransaction(transactionId);
-            };
-            this._table().children('tbody').html(html).children("tr.transaction").click(displayContributions);
+            this._table().children('tbody').html(html).children("tr.transaction").click(
+                (e) => {
+                    const tr = $(e.currentTarget);
+                    const transactionId = tr.attr("id");
+                    Fwk.show("Ingest", "Contributions");
+                    Fwk.current().loadTransaction(this._get_database(), this._transactions, transactionId);
+                }
+            );
 
         }
     }
